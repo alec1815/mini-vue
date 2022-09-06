8632f39c0789e2bed555eb4186b407dd80fda164
wzhaofei <wangzf@wellnkiot.com>
2022-09-06 11:24:33 +0800

    feat: 实现effect依赖收集、依赖触发

---
+++ src/reactivity/effect.ts
@@ -0,0 +1,44 @@
+class ReactiveEffect{
+    private _fn: any
+    constructor(fn){
+        this._fn = fn 
+    }
+    run(){
+        activeEffect = this
+        this._fn()
+    }
+}
+
+const tagertMap =  new Map()
+export function track(target,key){
+    let depsMap = tagertMap.get(target)
+    if(!depsMap){
+        depsMap = new Map()
+        tagertMap.set(target,depsMap)
+    }
+
+    let dep = depsMap.get(key)
+    if(!dep){
+        dep = new Set()
+        depsMap.set(key,dep)
+    }
+
+    dep.add(activeEffect)
+}
+
+export function trigger(tagert,key){
+    let depsMap  = tagertMap.get(tagert)
+    let dep = depsMap.get(key)
+
+    for(const effect of dep){
+        effect.run()
+    }
+}
+
+let activeEffect
+export function effect(fn){
+    // fn 
+    const _effect = new ReactiveEffect(fn)
+
+    _effect.run()
+}
