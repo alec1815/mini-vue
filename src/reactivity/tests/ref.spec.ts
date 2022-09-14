fb2034d174655d5cb830c54ac1d2a9eff9dc6d6e
wzhaofei <wangzf@wellnkiot.com>
2022-09-14 11:13:19 +0800

    feat: ref 测试用例

---
+++ src/reactivity/tests/ref.spec.ts
@@ -0,0 +1,29 @@
+import { effect } from "../effect";
+import { ref } from "../ref"
+
+describe("ref",()=>{
+    it("happy path", ()=>{
+        const num = ref(1)
+        expect(num.value).toBe(1)
+    })
+
+    it("should be reactive",()=>{
+        const num = ref(1)
+        let dummy
+        let calls = 0
+        effect(()=>{
+            calls++
+            dummy = num.value
+        })
+
+        expect(calls).toBe(1)
+        expect(dummy).toBe(1)
+        num.value = 2
+        expect(calls).toBe(2)
+        expect(dummy).toBe(2)
+
+        num.value = 2
+        expect(calls).toBe(2)
+        expect(dummy).toBe(2)
+    })
+})
