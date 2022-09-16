fb4c8e53b1625a668a020e8de852ee090aa889b0
wzhaofei <wangzf@wellnkiot.com>
2022-09-16 17:55:09 +0800

    feat: 实现初始化 element 主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,16 @@ import { h } from "../../lib/guid-mini-vue.es.js"
 
 export const App = {
     render() {
-        return h("div", "hi, " + this.msg)
+        return h(
+            "div",
+            {
+                id: "root",
+                class: ["red", "hard"]
+            },
+            [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")]
+            // "hi, mini-vue"
+            // "hi, " + this.msg
+        )
     },
     setup() {
         return {
--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,3 +1,4 @@
+import { isObject } from "../shared/index"
 import { createComponentInstance, setupComponent } from "./component"
 
 export function render(vnode,container){
@@ -8,13 +9,39 @@ export function render(vnode,container){
 
 function patch(vnode,container){
 
-    procesElement()
-
-    // 去处理组件
-    procesComponent(vnode,container)
+    if(typeof vnode.type === "string"){
+        procesElement(vnode,container)
+    }else if(isObject(vnode.type)){
+        // 去处理组件
+        procesComponent(vnode,container)
+    }
 }
 
-function procesElement(){}
+function procesElement(vnode,container){
+    mountElement(vnode,container)
+}
+
+function mountElement(vnode,container){
+    const { children, type, props } = vnode
+    const el = document.createElement(type)
+    if(typeof children === "string"){
+        el.textContent = children
+    }else if(Array.isArray(children)){
+        mountChildren(vnode,container)
+    }
+    
+    for (const key in props) {
+       const val = props[key]
+       el.setAttribute(key, val)
+    }
+    container.append(el)
+}
+
+function mountChildren(vnode,container){
+    vnode.children.forEach(v=>{
+        patch(v,container)
+    })
+}
 
 function procesComponent(vnode,container){
     mountComponent(vnode,container)
