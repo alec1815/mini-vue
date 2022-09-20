85deb88a172ec9e0ebaa125fa5184818ef7f73d4
wzhaofei <wangzf@wellnkiot.com>
2022-09-20 16:44:29 +0800

    fix: 给案例添加点击事件

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -10,7 +10,10 @@ export const App = {
             "div",
             {
                 id: "root",
-                class: ["red", "hard"]
+                class: ["red", "hard"],
+                onClick() {
+                    console.log("click")
+                }
             },
             // [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")]
             // "hi, mini-vue"
