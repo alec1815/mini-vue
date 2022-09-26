b39d8f1994ca6fcb50dd3901935c88915d742eea
wzhaofei <wangzf@wellnkiot.com>
2022-09-26 16:45:18 +0800

    chore: 组件事件

---
+++ example/componentEmit/App.js
@@ -0,0 +1,14 @@
+
+import { h } from "../../lib/guid-mini-vue.es.js"
+import { Foo } from "./Foo.js"
+
+export const App = {
+    name: "App",
+    render() {
+        // emit
+        return h("div", {}, [h("div", {}, "App"), h(Foo, {})])
+    },
+    setup() {
+        return {}
+    }
+}
---
+++ example/componentEmit/Foo.js
@@ -0,0 +1,20 @@
+import { h } from "../../lib/guid-mini-vue.es.js"
+
+export const Foo = {
+    setup(props, { emit }) {
+        const emitAdd = () => {
+            console.log("emit add")
+        }
+
+        return {
+            emitAdd
+        }
+    },
+    render() {
+        const btn = h("button", {
+            onClick: this.emitAdd
+        }, "emitAdd")
+        const foo = h("p", {}, "foo")
+        return h("div", {}, [foo, btn])
+    }
+}
