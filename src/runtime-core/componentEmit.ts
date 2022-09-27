6a1f60c3722ead194ee9f892b0f64593f439a472
wzhaofei <wangzf@wellnkiot.com>
2022-09-27 17:10:29 +0800

    fix: 实现通用的事件触发

--- src/runtime-core/componentEmit.ts
+++ src/runtime-core/componentEmit.ts
@@ -4,6 +4,15 @@ export function emit(instance,event:any){
     console.log("emit",event)
     const { props } = instance
 
-    const handler = props["onAdd"]
+    const capitalize = (str:string) =>{
+        return str.charAt(0).toLocaleUpperCase() + str.slice(1)
+    }
+
+    const toHandlerKey = (str:string) =>{
+        return str ? "on" +capitalize(event) : ""
+    }
+
+    const handlerName = toHandlerKey(event)
+    const handler = props[handlerName]
     handler && handler()
 }
